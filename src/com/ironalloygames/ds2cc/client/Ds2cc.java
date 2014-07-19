02e091428f08eeebb5ab981294176e4eac966cc1
Eric Brown <quadtree@gmail.com>
2014-07-18 17:41:05 -0700

    Initial commit

---
+++ src/com/ironalloygames/ds2cc/client/Ds2cc.java
@@ -0,0 +1,16 @@
+package com.ironalloygames.ds2cc.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Ds2cc implements EntryPoint {
+	/**
+	 * This is the entry point method.
+	 */
+	@Override
+	public void onModuleLoad() {
+	}
+
+}
