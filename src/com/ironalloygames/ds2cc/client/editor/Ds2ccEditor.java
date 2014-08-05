94860a32a357eab458ac0943b163adf2c81bb82e
Eric Brown <quadtree@gmail.com>
2014-08-04 19:34:07 -0700

    Item editor shows up

--- src/com/ironalloygames/ds2cc/client/editor/Ds2ccEditor.java
+++ src/com/ironalloygames/ds2cc/client/editor/Ds2ccEditor.java
@@ -1,12 +1,13 @@
 package com.ironalloygames.ds2cc.client.editor;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class Ds2ccEditor implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-
+		RootPanel.get("mainContent").add(new EditorPage());
 	}
 
 }
