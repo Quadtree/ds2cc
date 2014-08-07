27dfb71002a5a20db3efe86666b92f2e75c2f4ae
Eric Brown <quadtree@gmail.com>
2014-08-06 22:35:18 -0700

    Images now cached in memcachE

--- src/com/ironalloygames/ds2cc/server/ImageServlet.java
+++ src/com/ironalloygames/ds2cc/server/ImageServlet.java
@@ -1,6 +1,7 @@
 package com.ironalloygames.ds2cc.server;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.geronimo.mail.util.Base64;
 
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import com.ironalloygames.ds2cc.shared.data.Item;
 
 public class ImageServlet extends HttpServlet {
@@ -20,12 +23,27 @@ public class ImageServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// MemcacheService ms = MemcacheServiceFactory.getMemcacheService();
+		byte[] rawImageData = null;
+		String memcacheKey = "IMAGE_" + req.getParameter("itemName").replaceAll("[A-Za-z '^]", "");
+		MemcacheService ms = MemcacheServiceFactory.getMemcacheService();
 
-		Item item = ItemDataService.getInstance().readItem(req.getParameter("itemName"));
+		try {
+			rawImageData = (byte[])ms.get(memcacheKey);
+		} catch(Exception ex){
+			Logger.getGlobal().warning(ex.toString());
+		}
+
+		if (rawImageData == null && !ms.contains(memcacheKey))
+		{
+			Item item = ItemDataService.getInstance().readItem(req.getParameter("itemName"));
+
+			if (item != null) {
+				rawImageData = Base64.decode(item.getEncodedImageData().getBytes());
+			}
+		}
 
 		resp.setContentType("image/png");
-		resp.getOutputStream().write(Base64.decode(item.getEncodedImageData()));
+		resp.getOutputStream().write(rawImageData);
 	}
 
 }
